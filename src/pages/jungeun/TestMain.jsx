da480a11bdf66a2b020ca7574948c8afe618fbd5
DotoriDaegoolPower <lje3299@naver.com>
2025-07-14 12:07:59 +0900

    0714 관리자-로그인/로그아웃/접근제어 완료 받아서 올리기

---
+++ src/pages/jungeun/TestMain.jsx
@@ -0,0 +1,22 @@
+import { test } from "../../api/Auth";
+
+export default function TestMain(){
+    const handleTestBackend = async (e) => {
+        e.preventDefault();
+    
+        try {
+          const response = await test();
+          console.log("✅ 응답:", response);
+        } catch (error) {
+          console.error("API 요청 실패:", error.message || error);
+          // 예: 토스트 띄우거나 화면에 오류 표시 가능
+        }
+      };
+    
+      return (
+        <div>
+          <h1>관리자 로그인 후 이동하는 메인 페이지</h1>
+          <button onClick={handleTestBackend}>테스트 백엔드 호출</button>
+        </div>
+      );
+    }
