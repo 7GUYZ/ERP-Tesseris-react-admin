aed4bc272322394e23983529e5c056534952a866
DotoriDaegoolPower <lje3299@naver.com>
2025-07-25 12:59:29 +0900

    App.js, Index.js 수정

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter basename="/">
+  <BrowserRouter basename={process.env.REACT_APP_BASENAME || "/"}> 
     <App />
   </BrowserRouter>
 );
