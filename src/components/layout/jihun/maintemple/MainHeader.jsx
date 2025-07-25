266e15a7f4ce8857203e0af2e92fa8937717b2c0
hasol2117 <hasol211704@gmail.com>
2025-07-25 17:27:17 +0900

    0725 충돌 해결

--- src/components/layout/jihun/maintemple/MainHeader.jsx
+++ src/components/layout/jihun/maintemple/MainHeader.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { useNavigate } from "react-router-dom";
 import { logout } from "../../../../api/auth/JungeunAuth";
 import useAuthStore from "../../../../store/jungeun/AuthStore";
